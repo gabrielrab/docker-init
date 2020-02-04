8552c047cffa029abdfb0322ac086927bc22d76d
Gabriel Rabelo Camargos <rabelogabriel72@gmail.com>
2020-02-04 15:21:39 -0300

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const mysql = require("mysql");
+const mongoose = require("mongoose");
+
+//MySQL
+try {
+  const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password"
+    //database: "rapicare"
+  });
+
+  conn.connect(function(err) {
+    if (err) {
+      return console.error("error: " + err.message);
+    }
+
+    console.log("Connected to the MySQL server.");
+  });
+
+  conn.end(() => {
+    console.log("End connection");
+  });
+} catch (error) {
+  console.log("erro");
+}
+
+//MongoDB
+mongoose.connect(
+  "mongodb://root:root@localhost:27017",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  err => {
+    if (err) {
+      return console.error("error: " + err.message);
+    }
+    console.log("Conect mongo");
+  }
+);
