224039d1bd620594f2ac61805ce1e820e91d96b0
Gabriel Rabelo Camargos <rabelogabriel72@gmail.com>
2020-02-05 22:48:32 -0300

    cleanup

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ const mongoose = require("mongoose");
 //MySQL
 try {
   const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password"
-    //database: "rapicare"
+    host: "localhost", //your MYSQL host [127.0.0.1 || localhost is the default if running local]
+    user: "root", //your MYSQL user
+    password: "password", //your MYSQL password
+    port: 3306 //your MYSQL port [3306 default]
+    //database: "your database name" [optional]
   });
 
+  //init connection
   conn.connect(function(err) {
+    //If there is an error
     if (err) {
       return console.error("error: " + err.message);
     }
-
+    //Success connect
     console.log("Connected to the MySQL server.");
   });
-
+  //Close connect
   conn.end(() => {
     console.log("End connection");
   });
@@ -32,10 +35,12 @@ mongoose.connect(
     useNewUrlParser: true,
     useUnifiedTopology: true
   },
+  //If there is an error
   err => {
     if (err) {
       return console.error("error: " + err.message);
     }
+    //Success connect
     console.log("Conect mongo");
   }
 );
